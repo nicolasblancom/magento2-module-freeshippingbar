501a8d94beb5be111d96806280b3bbf40a7118b2
nicolasblancom <nicolasblancom@gmail.com>
2023-10-09 15:40:44 +0200

    Get cart amount

--- view/frontend/web/js/free-shipping-bar.js
+++ view/frontend/web/js/free-shipping-bar.js
@@ -1,17 +1,30 @@
 define([
-    'uiComponent'
+    'uiComponent',
+    'Magento_Customer/js/customer-data'
 ], function(
-    Component
+    Component,
+    customerData
 ) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            message: "Free shipping for orders above XX.XX!"
+            message: "Free shipping for orders above XX.XX!",
+            cartAmount: 0.00,
+            formattedCartAmount: '<span class="price">0.00</span>'
         },
         initialize: function () {
             this._super();
             const self = this;
+
+            // Get cart amount
+            const cart = customerData.get('cart');
+            customerData.getInitCustomerData().done(() => {
+                self.cartAmount = parseFloat(cart().subtotalAmount);
+                self.formattedCartAmount = cart().subtotal_excl_tax;
+            });
+
+
         }
     });
 });
